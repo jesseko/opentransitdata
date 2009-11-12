2d341e7dd9996dd8cd74eb85b0fb103d404b3648
Brandon Martin-Anderson <badhill@gmail.com>
2009-11-12 15:34:26 -0800

    deleted everything so importing dave's commit works

--- core/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
-
-def stub(request):
-    name = request.GET['name']
-    
-    return render_to_response( "stub.html", {'name':name} )
