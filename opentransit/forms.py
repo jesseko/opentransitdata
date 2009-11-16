29d4a15d877ad353c97cf491fd2275a4a6c8c478
Brandon Martin-Anderson <badhill@gmail.com>
2009-11-16 14:53:02 -0800

    agency edit page

--- opentransit/forms.py
+++ opentransit/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import PetitionModel
+from django.forms import ModelForm
+from .models import PetitionModel, Agency
 
 # NOTE DAVEPECK: this is an example only. Ignore as you see fit!
 
@@ -10,3 +11,19 @@ class PetitionForm(forms.Form):
     state = forms.CharField(max_length=16, min_length=2, label=u"State")
     country = forms.CharField(max_length=16, min_length=2, label=u"Country")
     
+class AgencyForm(forms.Form):
+    name            = forms.CharField()
+    short_name      = forms.CharField(required=False)
+    tier            = forms.IntegerField(required=False)
+    city            = forms.CharField(required=False)
+    state           = forms.CharField(max_length=2,required=False)
+    country         = forms.CharField(required=False)
+    postal_code     = forms.IntegerField(required=False)
+    address         = forms.CharField(required=False)
+    agency_url      = forms.URLField(required=False)
+    executive       = forms.CharField(required=False)
+    executive_email = forms.EmailField(required=False)
+    twitter         = forms.CharField(required=False)
+    contact_email   = forms.EmailField(required=False)
+    updated         = forms.DateTimeField(required=False)
+    phone           = forms.CharField(required=False)
